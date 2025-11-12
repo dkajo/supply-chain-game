3a78c552d2a3f130e82eb82168691f64bbcdb425
Kalma <jonathan.kalma@hotmail.com>
2025-11-12 18:35:28 +0100

    Add maintenance function
    Reduces the defect rate of the produced products.

--- level.gd
+++ level.gd
@@ -3,12 +3,9 @@ extends Node2D
 var product_scene: PackedScene = load("res://product.tscn") # Enable products to be instantiated within this scene
 var balance: int = 0 # Score tracker
 var produced_count := 0 # Counts the number of prroducts produced.
-@export var production_limit := 20 # Limit how many to produce before stopping.
-
-# 20% chance of a product being defect.
-@export_range (0.0, 1.0, 0.01) var defect_chance := 0.2 
+@export var production_limit := 10 # Limit how many to produce before stopping.
+ 
 var rng = RandomNumberGenerator.new()
-
 
 func _ready():
 	rng.randomize()
@@ -16,19 +13,28 @@ func _ready():
 	$Truck.product_entered.connect(_on_truck_loaded_product)
 
 # Production timer controls production and is thereby the primary game engine
-func _on_production_timer_timeout():
-	# Check production count before creating the next product
+func _on_production_timer_timeout(): # Holds everything that needs to happen as a product is produced
+	# Check production limit before producing the next product
 	if produced_count >= production_limit:
 		$ProductionTimer.stop()
 		return
 	 
 	var product = product_scene.instantiate() # Create an instance of a product
-	product.is_defect = rng.randf() < defect_chance # Evaluates to a boolean
-	add_child(product) # Attach node to scene tree
+	product.is_defect = $Machine.roll_is_defect(rng) # Defect or not decided by machine
+	
+	add_child(product) # Attach node to scene tree, adds product to level.
 	produced_count += 1 # Increment after produced product.
 	
 
-# Button Signals
+# --- Money Helpers ---
+func can_afford(price: int) -> bool:
+	return balance >= price
+
+func spend(price: int) -> void:
+	balance -= price
+	$UI/Control/BalanceLabel.text = "Balance: %d" % balance
+
+# --- Button Signals ---
 func _on_start_button_pressed() -> void:
 	produced_count = 0 # Reset the produced count
 	balance = 0 # Reset the balance
@@ -38,7 +44,15 @@ func _on_start_button_pressed() -> void:
 func _on_stop_button_pressed() -> void:
 	$ProductionTimer.stop()
 
+func _on_maintenance_btn_pressed() -> void:
+	if can_afford($Machine.get_maintenance_cost()):
+		spend($Machine.get_maintenance_cost())
+		$Machine.apply_maintenance()
+	else:
+		print("Not enough balance")
+
 # Fired when a product enters the truck, and simulates a product being sold
 func _on_truck_loaded_product(product): # Receive the product from the signal
 	balance += product.value
 	$UI/Control/BalanceLabel.text = "Balance: %d" % balance
+	
---
+++ machine.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# 20% chance of a product being defect.
+@export_range (0.0, 1.0, 0.01) var defect_chance := 0.6
+
+# --- Maintenance ---
+@export var maintenance_cost := 50
+@export var maintenance_improvement := 0.5 # Improves defect chance by 50%
+
+func get_maintenance_cost() -> int:
+	return maintenance_cost
+
+signal maintenance_applied()
+
+func roll_is_defect (rng: RandomNumberGenerator) -> bool:
+	return rng.randf() < defect_chance
+
+func apply_maintenance() -> void:
+	defect_chance *= maintenance_improvement
+	print("Defect chance: ", defect_chance)
+	emit_signal("maintenance_applied")
