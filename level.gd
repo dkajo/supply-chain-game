f357e675725603cd89130935fe996fee19a3b5a1
Kalma <jonathan.kalma@hotmail.com>
2025-11-20 20:14:12 +0100

    Removed turn-based implementation
    I did not like the feel of turns

--- level.gd
+++ level.gd
@@ -3,10 +3,6 @@ extends Node2D
 var product_scene: PackedScene = load("res://product.tscn") # Enable products to be instantiated within this scene
 var balance: int = 0 # Score tracker
 var produced_count := 0 # Counts the number of products produced.
-var turn := 0 # Keeps track of the runs
-var is_active_turn := false # Boolean keeps track of active turn
-@export var max_turn := 5 # How many turns until Game Over
-@export var production_limit := 10 # Limit how many to produce before stopping.
  
 var rng = RandomNumberGenerator.new()
 
@@ -17,17 +13,11 @@ func _ready():
 
 # Production timer controls production and is thereby the primary game engine
 func _on_production_timer_timeout(): # Holds everything that needs to happen as a product is produced
-	# Check production limit before producing the next product
-	if produced_count >= production_limit:
-		$ProductionTimer.stop()
-		return
-	 
 	var product = product_scene.instantiate() # Create an instance of a product
 	product.is_defect = $Machine.roll_is_defect(rng) # Defect or not decided by machine
 	
 	add_child(product) # Attach node to scene tree, adds product to level.
 	produced_count += 1 # Increment after produced product.
-	
 
 # --- Money Helpers ---
 func can_afford(price: int) -> bool:
@@ -39,8 +29,6 @@ func spend(price: int) -> void:
 
 # --- Button Signals ---
 func _on_start_button_pressed() -> void:
-	produced_count = 0 # Reset the produced count
-	balance = 0 # Reset the balance
 	$UI/Control/BalanceLabel.text = "Balance: %d" % balance
 	$ProductionTimer.start()
 
